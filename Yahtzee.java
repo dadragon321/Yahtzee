c01bed92a0663dcfa11623abc892b2b5374b1519
spencez <36173887+spencez@users.noreply.github.com>
2022-04-27 11:01:33 -0400

    list

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,26 +1,29 @@
+import java.util.ArrayList;
+
 public class Yahtzee {
-  private int[] dice;
+  private ArrayList<Integer> dice;
 
   public static void main(String[] args) {
     Yahtzee game = new Yahtzee();
-    game.roll();
+    game.roll(7);
     for(int val:game.getDice())
       System.out.println(val);
   }
 
   public Yahtzee() {
-    dice = new int[5];
+    dice = new ArrayList<Integer>();
   }
 
-  public void roll() {
-    for(int i=0; i<5; i++)
-      dice[i] = (int)(Math.random()*6 + 1);
+  public void roll(int n) {
+    for(int i=0; i<n; i++)
+      dice.add((int)(Math.random()*6 + 1));
   }
 
-  private int score(int[] roll) {
+  private int score(ArrayList<Integer> roll) {
+    return 0;
   }
 
-  public int[] getDice() {
+  public ArrayList<Integer> getDice() {
     return dice;
   }
 }
