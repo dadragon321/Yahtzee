214882e441818e84636c2238544909705868d1ec
spencez <36173887+spencez@users.noreply.github.com>
2022-04-27 10:29:22 -0400

    init

---
+++ Yahtzee.java
@@ -0,0 +1,15 @@
+public class Yahtzee {
+  public static void main(String[] args) {
+    Yahtzee game = new Yahtzee();
+    int[] dice = game.roll();
+    for(int val:dice)
+      System.out.println(val);
+  }
+
+  public int[] roll() {
+    int[] dice = new int[5];
+    for(int i=0; i<5; i++)
+      dice[i] = (int)(Math.random()*6 + 1);
+    return dice;
+  }
+}
