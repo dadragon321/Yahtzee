68dcc3150ad55c88f5d38aa1b5047a3d5713423b
spencez <36173887+spencez@users.noreply.github.com>
2022-04-27 11:20:08 -0400

    Update Yahtzee.java

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Yahtzee {
+  private Scanner sc;
   private ArrayList<Integer> dice;
 
   public static void main(String[] args) {
@@ -11,7 +13,19 @@ public class Yahtzee {
   }
 
   public Yahtzee() {
+    sc = new Scanner(System.in);
     dice = new ArrayList<Integer>();
+  }
+
+  public turn() {
+    game.roll(6);
+    int drop;
+    while(i !=0 && dice.size()>0) {
+      System.out.print("Choose which dice to reroll (Enter 0 to move to reroll): ");
+      drop = sc.nextInt();
+      if(drop>0 && drop<dice.size()+1)
+        dice.remove(drop+1);
+    }
   }
 
   public void roll(int n) {
